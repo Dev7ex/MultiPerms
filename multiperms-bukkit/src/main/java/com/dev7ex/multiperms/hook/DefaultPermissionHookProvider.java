7f42ae2d7c128a026a6be965d0f9df51620163ee
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-11-17 20:48:06 +0100

    Update DefaultPermissionHookProvider.java

--- multiperms-bukkit/src/main/java/com/dev7ex/multiperms/hook/DefaultPermissionHookProvider.java
+++ multiperms-bukkit/src/main/java/com/dev7ex/multiperms/hook/DefaultPermissionHookProvider.java
@@ -1,35 +1,42 @@
 package com.dev7ex.multiperms.hook;
 
-import com.dev7ex.common.bukkit.BukkitCommonPlugin;
 import com.dev7ex.common.bukkit.plugin.module.PluginModule;
 import com.dev7ex.multiperms.MultiPermsPlugin;
-import com.dev7ex.multiperms.api.bukkit.hook.BukkitPermissionHook;
+import com.dev7ex.multiperms.api.bukkit.hook.BukkitPermissionHookProvider;
 import com.dev7ex.multiperms.api.hook.PermissionHook;
-import com.dev7ex.multiperms.api.hook.PermissionHookProvider;
+import com.dev7ex.multiperms.api.hook.defaults.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Dev7ex
  * @since 03.08.2023
  */
 @Getter(AccessLevel.PUBLIC)
-public class DefaultPermissionHookProvider implements PluginModule, PermissionHookProvider<Plugin> {
+public class DefaultPermissionHookProvider implements PluginModule, BukkitPermissionHookProvider {
 
-    private final Map<Plugin, PermissionHook> permissionHooks = new HashMap<>();
+    private final Map<Plugin, List<PermissionHook>> permissionHooks = new HashMap<>();
 
     @Override
     public void onEnable() {
-        this.register(MultiPermsPlugin.getInstance(), new MultiPermsHook());
-        this.register(JavaPlugin.getPlugin(BukkitCommonPlugin.class), new BukkitPermissionHook());
+        final MultiPermsPlugin hookHolder = MultiPermsPlugin.getInstance();
+        final List<PermissionHook> multiPermsHooks = List.of(new BukkitPermissionHook(),
+                new MinecraftPermissionHook(),
+                new MinecraftPermissionHook(),
+                new MultiPermsHook(),
+                new PaperPermissionHook(),
+                new VanillaPermissionHook());
+
+        this.permissionHooks.put(hookHolder, multiPermsHooks);
+
+        MultiPermsPlugin.getInstance().getLogger().info("Found: [" + this.permissionHooks.values().stream()
+                .mapToLong(List::size)
+                .sum() + "] PermissionHooks");
     }
 
     @Override
@@ -39,19 +46,51 @@ public class DefaultPermissionHookProvider implements PluginModule, PermissionHo
 
     @Override
     public void register(@NotNull final Plugin hookHolder, @NotNull final PermissionHook hook) {
-        MultiPermsPlugin.getInstance().getLogger().info("Register Hook " + hook.getClass().getSimpleName());
-        this.permissionHooks.put(hookHolder, hook);
+        this.permissionHooks.computeIfAbsent(hookHolder, plugin -> new ArrayList<>());
+        final List<PermissionHook> hooks = this.permissionHooks.get(hookHolder);
+        hooks.add(hook);
+
+        this.permissionHooks.put(hookHolder, hooks);
+        hook.register();
+
+        MultiPermsPlugin.getInstance()
+                .getLogger()
+                .info("Register [" + this.permissionHooks.get(hookHolder).size() + "] Permission Hooks from"
+                        + hookHolder.getClass().getSimpleName());
     }
 
     @Override
     public void unregister(@NotNull final Plugin hookHolder) {
+        final List<PermissionHook> hooks = this.permissionHooks.get(hookHolder);
+
+        if ((hooks == null) || (hooks.isEmpty())) {
+            return;
+        }
+        this.permissionHooks.get(hookHolder).forEach(PermissionHook::unregister);
         this.permissionHooks.remove(hookHolder);
+
+        MultiPermsPlugin.getInstance()
+                .getLogger()
+                .info("Unregister [" + this.permissionHooks.get(hookHolder).size() + "] Permission Hooks from"
+                        + hookHolder.getClass().getSimpleName());
     }
 
-    public List<String> getAllPermissions() {
-        final List<String> permissions = new ArrayList<>();
-        this.permissionHooks.values().forEach(permissionHook -> permissions.addAll(permissionHook.getPermissions()));
-        return permissions;
+    @Override
+    public boolean isRegistered(@NotNull final Plugin hookHolder) {
+        return this.permissionHooks.containsKey(hookHolder);
+    }
+
+    @Override
+    public List<PermissionHook> getHooks(@NotNull final Plugin hookHolder) {
+        return this.permissionHooks.get(hookHolder);
+    }
+
+    @Override
+    public List<String> getRegisteredPermissions() {
+        return permissionHooks.values().stream()
+                .flatMap(Collection::stream)
+                .flatMap(permissionHook -> permissionHook.getPermissions().stream())
+                .collect(Collectors.toList());
     }
 
 }
