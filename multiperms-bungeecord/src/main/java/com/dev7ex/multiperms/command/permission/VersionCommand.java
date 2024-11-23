144438c0e973104aab0045d441bdbc90f549a1d9
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-11-23 20:39:00 +0100

    Create VersionCommand.java

---
+++ multiperms-bungeecord/src/main/java/com/dev7ex/multiperms/command/permission/VersionCommand.java
@@ -0,0 +1,40 @@
+package com.dev7ex.multiperms.command.permission;
+
+import com.dev7ex.common.bungeecord.command.BungeeCommand;
+import com.dev7ex.common.bungeecord.command.BungeeCommandProperties;
+import com.dev7ex.common.bungeecord.plugin.BungeePlugin;
+import com.dev7ex.multiperms.MultiPermsPlugin;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dev7ex
+ * @since 23.11.2024
+ */
+@BungeeCommandProperties(name = "version", permission = "multiperms.command.permission.version")
+public class VersionCommand extends BungeeCommand {
+
+    public VersionCommand(@NotNull final BungeePlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void execute(@NotNull final CommandSender commandSender, @NotNull final String[] arguments) {
+        final MultiPermsPlugin plugin = MultiPermsPlugin.getInstance();
+
+        commandSender.sendMessage(new TextComponent(" "));
+        commandSender.sendMessage(new TextComponent("§f§m                    §r§r " + super.getConfiguration().getPrefix() + " §f§m                    "));
+        commandSender.sendMessage(new TextComponent(" "));
+        commandSender.sendMessage(new TextComponent("§8» §2Version: §a" + plugin.getDescription().getVersion()));
+        commandSender.sendMessage(new TextComponent("§8» §2Authors: " + plugin.getDescription().getAuthor()));
+        commandSender.sendMessage(new TextComponent("§8» §2Support: §ahttps://discord.gg/ta33bbA8eF"));
+        commandSender.sendMessage(new TextComponent("§8» §2Wiki: §ahttps://github.com/Dev7ex/MultiPerms/wiki"));
+        commandSender.sendMessage(new TextComponent("§8» §2Report Bug: §ahttps://github.com/Dev7ex/MultiPerms/issues"));
+        commandSender.sendMessage(new TextComponent(" "));
+        commandSender.sendMessage(new TextComponent("§f§m                    §r§r " + super.getConfiguration().getPrefix() + " §f§m                    "));
+        commandSender.sendMessage(new TextComponent(" "));
+    }
+
+}
